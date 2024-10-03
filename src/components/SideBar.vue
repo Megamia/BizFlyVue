5f89268c4cf8a6d426f694ba6788435c483f2aba
Megamia <luudat214@gmaill.com>
2024-10-03 10:50:21 +0700

    update

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,21 +3,36 @@
     id="dddddd"
     v-model:openKeys="openKeys"
     v-model:selectedKeys="selectedKeys"
-    style="width: 256px"
+    :style="{ width: isMenuOpen ? '80px' : '256px' }"
     mode="inline"
     :items="items"
+    :inline-collapsed="isMenuOpen"
     @click="handleClick"
   >
   </a-menu>
+  <button @click="toggleMenu">{{ isMenuOpen ? "Open" : "Close" }}</button>
 </template>
 <script setup>
-import { reactive, ref, watch, h } from "vue";
+import { reactive, ref, h } from "vue";
 import {
-  MailOutlined,
-  AppstoreOutlined,
+  HomeOutlined,
+  UsergroupAddOutlined,
+  ApartmentOutlined,
+  FileTextOutlined,
+  DollarOutlined,
+  GiftOutlined,
   SettingOutlined,
+  // MailOutlined,
+  // AppstoreOutlined,
+  // SettingOutlined,
 } from "@ant-design/icons-vue";
 import { useRouter } from "vue-router";
+
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
 
 const router = useRouter();
 const selectedKeys = ref(["1"]);
@@ -32,60 +47,44 @@ function getItem(label, key, icon, children, type) {
   };
 }
 const items = reactive([
-  getItem("Navigation One", "sub1", () => h(MailOutlined), [
-    getItem(
-      "Item 1",
-      "g1",
-      null,
-      [getItem("Option 1", "1"), getItem("Option 2", "2")],
-      "group"
-    ),
-    getItem(
-      "Item 2",
-      "g2",
-      null,
-      [getItem("Option 3", "3"), getItem("Option 4", "4")],
-      "group"
-    ),
+  getItem("Trang chủ", "sub1", () => h(HomeOutlined)),
+  getItem("Thành viên", "sub2", () => h(UsergroupAddOutlined)),
+  getItem("Phân hạng thành viên", "sub3", () => h(ApartmentOutlined)),
+  // {
+  //   type: "divider", //Đường kẻ phân chia giữa các item
+  // },
+  getItem("Phiếu điều chỉnh", "sub4", () => h(FileTextOutlined), [
+    getItem("Điều chỉnh điểm", "1"),
+    getItem("Điều chỉnh hạng", "2"),
   ]),
-  getItem("Navigation Two", "sub2", () => h(AppstoreOutlined), [
-    getItem("Option 5", "5"),
-    getItem("Option 6", "6"),
-    getItem("Submenu", "sub3", null, [
-      getItem("Option 7", "7"),
-      getItem("Option 8", "8"),
-    ]),
+  getItem("Tích điểm", "sub5", () => h(DollarOutlined), [
+    getItem("Chương trình tích điểm", "3"),
+    getItem("Giao dịch tích điểm", "4"),
   ]),
-  {
-    type: "divider",
-  },
-  getItem("Navigation Three", "sub4", () => h(SettingOutlined), [
-    getItem("Option 9", "9"),
-    getItem("Option 10", "10"),
-    getItem("Option 11", "11"),
-    getItem("Option 12", "12"),
+  getItem("Công cụ khuyến mãi", "sub6", () => h(GiftOutlined), [
+    getItem("Quản lý chương trình", "5"),
+    getItem("Quản lý chiến dịch", "6"),
+    getItem("Gạch mã", "7"),
   ]),
-  getItem(
-    "Group",
-    "grp",
-    null,
-    [getItem("Option 13", "13"), getItem("Option 14", "14")],
-    "group"
-  ),
+  getItem("Cài đặt", "sub7", () => h(SettingOutlined)),
+
+  // getItem(
+  //   "Group",
+  //   "grp",
+  //   null,
+  //   [getItem("Option 13", "13"), getItem("Option 14", "14")],
+  //   "group"
+  // ),
 ]);
 
 const handleClick = (e) => {
   console.log("click", e);
-  if (e.key === "1") {
+  if (e.key === "sub1") {
     router.push("/AddNewProgram");
-  } else if (e.key === "2") {
+  } else if (e.key === "sub2") {
     router.push("/HomePage");
-  } else if (e.key === "3") {
+  } else if (e.key === "sub3") {
     router.back();
   }
 };
-
-watch(openKeys, (val) => {
-  console.log("openKeys", val);
-});
 </script>
